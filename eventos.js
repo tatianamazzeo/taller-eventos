7693eacca3ee1be8b1c64909921d99d739539f3e
Tatiana Mazzeo <163008890+tatianamazzeo@users.noreply.github.com>
2024-08-10 19:19:26 -0300

    Agrego manejador de eventos del div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const divBoton = document.getElementById("boton");
+
+  divBoton.addEventListener("click", function () {
+    alert("Hola! soy el div");
+  });
+});
